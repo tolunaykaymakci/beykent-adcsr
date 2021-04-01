ac38f261d7f23072a3daf58af2368e329aecd1b3
unknown <havvanursezer0@gmail.com>
2021-04-01 23:01:48 +0300

    plan detayları

--- ClientReactNative/screens/PlanDetailsScreen.js
+++ ClientReactNative/screens/PlanDetailsScreen.js
@@ -1,8 +1,11 @@
 ﻿import React, {useEffect, useState} from 'react';
-import {View, Text, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet, ScrollView, Image, Dimensions} from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {authorizedRequest} from '../Service';
+
+const width = Dimensions.get("window").width;
 
 const DetailsScreen = ({route, navigation}) => {
   const {planId} = route.params;
@@ -20,74 +23,134 @@ const DetailsScreen = ({route, navigation}) => {
       .finally(() => {});
   }, []);
 
+  const onDayPlan = () =>{
+    navigation.navigate('StudiesPlan', {plan: plan})
+  }
+
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1, width:'100%', height:'100%'}}>
       {plan == null ? (
         <Text>Yükleniyor...</Text>
       ) : (
-        <ScrollView>
-          <View style={{flexDirection: 'row'}}></View>
-
-          <View style={{flexDirection: 'row', flex: 1}}>
-            <View style={{backgroundColor: '#f00', height: 24, flex: 1}}>
-              <Text style={{alignSelf: 'center'}}>Text1</Text>
-            </View>
-            <View style={{backgroundColor: '#0f0', height: 24, flex: 1}}>
-              <Text>Text2</Text>
-            </View>
-            <View style={{backgroundColor: '#00f', height: 24, flex: 1}}>
-              <Text>Text3</Text>
-            </View>
+        <View style={{alignItems:"center", width:'100%', height:'100%'}}>
+          <View style={{
+            justifyContent:"space-around",
+            alignItems:"center",
+            backgroundColor:"#A8DEFF",
+            width:'90%',
+            height:'20%',
+            padding:'3%',
+            borderRadius:20,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.50,
+            shadowRadius: 20,
+            elevation: 10,
+          }}>
+              <View style={{width:'90%', flexDirection:"row", alignItems:"center", justifyContent:"space-between"}} >
+                <Image 
+                    source={require("../assest/exam.png")}
+                    style={{width:30, height:30}}
+                    resizeMode="contain"
+                />
+                <Text style={{fontSize:16, color:"#003351"}}>
+                  {plan.exam_title}
+                </Text>
+              </View>
+              <View style={{width:'90%', flexDirection:"row", alignItems:"center", justifyContent:"space-between"}} >
+                <Image 
+                    source={require("../assest/name.png")}
+                    style={{width:30, height:30}}
+                    resizeMode="contain"
+                />
+                <Text style={{fontSize:16, color:"#003351"}}>
+                    {plan.name}
+                </Text>
+              </View>
+              <View style={{width:'90%', flexDirection:"row", alignItems:"center", justifyContent:"space-between"}} >
+                <Image 
+                    source={require("../assest/calendar.png")}
+                    style={{width:30, height:30}}
+                    resizeMode="contain"
+                />
+                <Text style={{fontSize:16, color:"#003351"}}>
+                    {plan.created}
+                </Text>
+              </View>
           </View>
-
-          <Text style={{fontSize: 13, fontWeight: 'bold', padding: 12}}>
-            PLAN DETAYLARI
-          </Text>
-
-          <View style={styles.osman}>
-            <Text style={{color: 'rgb(34,34,34)', fontWeight: 'bold'}}>
-              Çalıştığınız Sınav
-            </Text>
-            <Text style={{color: 'rgb(70,70,70)'}}>{plan.exam_title}</Text>
+          <View style={{
+            justifyContent:"space-around",
+            backgroundColor:"#A8DEFF",
+            padding:'3%',
+            width:'90%',
+            height:'20%',
+            marginTop:20,
+            borderRadius:20,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.50,
+            shadowRadius: 20,
+            elevation: 10,
+          }}>
+              <Text style={{fontSize:16, color:"#003351"}}>
+                Ders Programı
+              </Text>
+              <Text style={{fontSize:13, color:"#003351"}}>
+                  Bir ders programı oluşturarak kendinize günlük soru hedefleri belirleyebilirsiniz. Soru sayacı hedefinize ulaşmanıza yardımcı olacaktır.
+              </Text>
+              <TouchableOpacity style={{width:'100%', flexDirection:"row", alignItems:"center", justifyContent:"flex-end"}} onPress={onDayPlan} >
+                <Image 
+                    source={require("../assest/notebook.png")}
+                    style={{width:30, height:30}}
+                    resizeMode="contain"
+                />
+                <Text style={{fontSize:16, color:"#003351"}}>
+                    Günlük Ders Programı
+                </Text>
+              </TouchableOpacity>
           </View>
+          <FlatList 
+            data={plan.lessons}
+            numColumns={2}
+            contentContainerStyle={{width:width, justifyContent:"space-around", marginTop:20}}
+            columnWrapperStyle={{justifyContent:"space-around", width:'98%', marginBottom:20}}
+            keyExtractor={item => item.lesson_id}
+            renderItem={({item, index})=>(
+                <View key={index} style={{
+                  justifyContent:"space-around",
+                    alignItems:"center",
+                    backgroundColor:"#E1F4FF",
+                    width:width*40/100,
+                    height:width*40/100,
+                    borderRadius:20,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                      width: 0,
+                      height: 2,
+                    },
+                    shadowOpacity: 0.50,
+                    shadowRadius: 20,
+                    elevation: 10,
+                }} >
+                    <Image 
+                      source={require("../assest/study.png")}
+                      style={{width:50, height:50}}
+                      resizeMode="contain"
+                    />
+                    <Text style={{fontSize:14, color:"#003351"}}>
+                      {item.name}
+                    </Text>
+                </View>
+            )}
+          />
+        </View>
 
-          <View style={styles.osman}>
-            <Text style={{color: 'rgb(34,34,34)', fontWeight: 'bold'}}>
-              Plan İsmi
-            </Text>
-            <Text style={{color: 'rgb(70,70,70)'}}>{plan.name}</Text>
-
-            <MaterialCommunityIcons
-              style={{
-                position: 'absolute',
-                right: 0,
-                marginTop: 24,
-                marginEnd: 12,
-              }}
-              onPress={() => alert('merhaba!')}
-              name="border-color"
-              color={'rgb(64,64,64)'}
-              size={22}
-            />
-          </View>
-
-          <View style={{backgroundColor: 'white', padding: 12}}>
-            <Text style={{color: 'rgb(34,34,34)', fontWeight: 'bold'}}>
-              Oluşturulma Tarihi
-            </Text>
-            <Text style={{color: 'rgb(70,70,70)'}}>{plan.created}</Text>
-          </View>
-
-          <Text style={{fontSize: 13, fontWeight: 'bold', padding: 12}}>
-            DERSLERİM
-          </Text>
-
-          {plan.lessons.map((lesson, index) => (
-            <View style={{backgroundColor: 'white', padding: 12}}>
-              <Text>{lesson.name}</Text>
-            </View>
-          ))}
-        </ScrollView>
       )}
     </SafeAreaView>
   );
